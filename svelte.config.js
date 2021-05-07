1daca3f19d0b2c891f207796cfa015e5159f43fe
AndrewLester <alester220@gmail.com>
2021-05-07 16:12:21 -0400

    Add svelte-preprocess dependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import firebaseAdapter from 'svelte-adapter-firebase';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		adapter: firebaseAdapter()
 	}
