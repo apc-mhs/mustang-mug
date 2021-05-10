816ac1cf86c7cca36db6d4cec5ec6170748e1e31
AndrewLester <alester220@gmail.com>
2021-05-10 14:58:42 -0400

    Add basic error page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,19 @@
-<slot></slot>
+<script>
+import '../app.css';
+</script>
+
+<main>
+    <slot />
+</main>
+
+<style>
+:root {
+    --header-height: 50px;
+}
+
+main {
+    position: relative;
+    top: var(--header-height);
+    min-height: calc(100vh - var(--header-height));
+}
+</style>
