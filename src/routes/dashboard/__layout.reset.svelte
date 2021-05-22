04d8b1089708cc8804d4f5a520d37943623ab95d
Andrew Lester <alester220@gmail.com>
2021-05-21 20:25:18 -0400

    Add slot to dashboard layout file

--- src/routes/dashboard/__layout.reset.svelte
+++ src/routes/dashboard/__layout.reset.svelte
@@ -1,6 +1,9 @@
 <script>
-// TODO: Perform admin signin here
+// TODO: Perform admin signin here. This layout page should only perform signin and nothing else
+// The dashboard page content will go in dashboard/index.svelte
 </script>
+
+<slot />
 
 <style>
 </style>
