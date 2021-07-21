2a513e4a91ce2cc6d3207a972f374276054e10f9
AndrewLester <alester220@gmail.com>
2021-07-20 22:22:49 -0400

    Change MSB IDS

--- src/lib/msb/index.js
+++ src/lib/msb/index.js
@@ -1,9 +1,9 @@
 import { ApiClient } from 'msb_pay_api';
 
 const clientId = 'CP_5101290_test';
-const departmentId = 'msbpay_api_dept';
+const departmentId = 'food_services';
 const storeId = 'food_services_store';
-const paymentMethodId = 'msbpay_api';
+const paymentMethodId = 'food_services';
 const baseURL = 'https://test.www.myschoolbucks.com/msbpay/v2';
 const client = new ApiClient();
 client.basePath = baseURL;
